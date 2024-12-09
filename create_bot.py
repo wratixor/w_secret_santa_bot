c4e7641f4352adb6250ef2d37537813c9a1bd672
wratixor <wratixor@gmail.com>
2024-12-10 01:42:42 +0300

    Complete!

--- create_bot.py
+++ create_bot.py
@@ -5,7 +5,7 @@ from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 from decouple import config
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 storage: MemoryStorage = MemoryStorage()
 
 admins: set = set(int(admin_id) for admin_id in config('ADMINS').split(','))
